611f3bbb92d1452ad616c22a9fcd35339101a10c
Solara <fbp3sgxt@students.codeinstitute.net>
2024-12-17 11:08:32 +0000

    install allauth templates

--- env.py
+++ env.py
@@ -1,7 +1,7 @@
 import os
-os.environ["DATABASE_URL"] = "postgresql://neondb_owner:7yPcC3KVugdr@ep-dark-grass-a2a5hpbo.eu-central-1.aws.neon.tech/photo_shaky_debit_101173"
+os.environ["DATABASE_URL"] = "postgresql://neondb_owner:7yPcC3KVugdr@ep-dark-grass-a2a5hpbo.eu-central-1.aws.neon.tech/dose_ahead_judge_413576"
 os.environ["SECRET_KEY"] = "c>L*kz#qFSPf4vb*]A)JlwR2S39|iM"
 os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:7yPcC3KVugdr@ep-dark-grass-a2a5hpbo.eu-central-1.aws.neon.tech/photo_shaky_debit_101173")
+    "DATABASE_URL", "postgresql://neondb_owner:7yPcC3KVugdr@ep-dark-grass-a2a5hpbo.eu-central-1.aws.neon.tech/dose_ahead_judge_413576")
 os.environ.setdefault(
     "SECRET_KEY", "c>L*kz#qFSPf4vb*]A)JlwR2S39|iM")
