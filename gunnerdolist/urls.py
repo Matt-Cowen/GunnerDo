ad6569ba5e7a71c24c794073e75aca71aa1a4d70
Matthew Cowen <svlnsr3f@students.codeinstitute.net>
2024-12-17 11:52:54 +0000

    All pages linked in navbar

--- gunnerdolist/urls.py
+++ gunnerdolist/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 urlpatterns = [
       path('', views.HomeView.as_view(), name="home"),
       path('taskpage/', views.task_details, name='tasks'),
-      path('update_task/<str:pk>/', views.update_task, name='update task'),
-      path('delete_task/<str:pk>/', views.delete_task, name='delete task')
+      path('update_task/<str:pk>/', views.update_task, name='update_task'),
+      path('delete_task/<str:pk>/', views.delete_task, name='delete_task')
 ]
