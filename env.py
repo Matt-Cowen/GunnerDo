fa9fa24842031c2d95e57d850cdbafcbd96b7c39
Solara <fbp3sgxt@students.codeinstitute.net>
2024-12-17 10:42:33 +0000

    add env.py, begin implementing allauth

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+os.environ["DATABASE_URL"] = "postgresql://neondb_owner:7yPcC3KVugdr@ep-dark-grass-a2a5hpbo.eu-central-1.aws.neon.tech/photo_shaky_debit_101173"
+os.environ["SECRET_KEY"] = "c>L*kz#qFSPf4vb*]A)JlwR2S39|iM"
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:7yPcC3KVugdr@ep-dark-grass-a2a5hpbo.eu-central-1.aws.neon.tech/photo_shaky_debit_101173")
+os.environ.setdefault(
+    "SECRET_KEY", "c>L*kz#qFSPf4vb*]A)JlwR2S39|iM")
